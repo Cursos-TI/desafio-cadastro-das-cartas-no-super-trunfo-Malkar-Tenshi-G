bde9f8f4f7ee1a2135f953a20b7378a923a5d52d
Malkar-Tenshi-G <gabrielmdjam@gmail.com>
2025-02-26 11:50:59 -0400

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste Gabriel
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char nome_cidade1[100], nome_cidade2[100];
+    int populacao1, populacao2;
+    float area1, area2;
+    int pontos_turisticos1, pontos_turisticos2;
+    float pib1, pib2;
 
-    return 0;
-}
+    //Primeira Carta
+    printf("Cadastro da Carta 1:\n");
+    printf("Estado (ex: AM, SP, RJ...): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", nome_cidade');
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área em km²: ");
+    scanf("%f", &area1);
+
+    print("Pib: ");
+    scanf("%f", &pib1);
+
+    print("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+
+
+    //Segunda Carta
+    printf("Cadastro da Carta 2:\n);
+    printf("Estado (ex: AM, SP, RJ...): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta (ex: A01): ");
